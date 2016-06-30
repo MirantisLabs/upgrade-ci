dc21e502fb9764ec09b8e76bbf02a049dee8daec
metacoma <sryabin@mirantis.com>
2016-06-30 16:16:43 +0400

    Update upgrade_7_8_cluster.sh
    Add patchset for octane stable/8.0

--- jobs/upgrade_7_8_cluster.sh
+++ jobs/upgrade_7_8_cluster.sh
@@ -55,7 +55,7 @@ export OCTANE_PATCHES="$STABLE8_PATCHES"
 
 
 rm -rf fuel-qa8.0
-git_change_request https://github.com/openstack/fuel-qa stable/8.0 fuel-qa8.0 321611
+git_change_request https://github.com/openstack/fuel-qa stable/8.0 fuel-qa8.0 335380 321611
 cd fuel-qa8.0
 
 pip install -r fuelweb_test/requirements.txt --upgrade
