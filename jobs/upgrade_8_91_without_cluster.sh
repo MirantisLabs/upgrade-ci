059f84c6a6108aaecc8e00e16270beb7419768c3
metacoma <sryabin@mirantis.com>
2016-07-05 21:05:20 +0400

    Update upgrade_8_91_without_cluster.sh
    Add ability to set patchsets for fuel-qa

--- jobs/upgrade_8_91_without_cluster.sh
+++ jobs/upgrade_8_91_without_cluster.sh
@@ -45,7 +45,7 @@ export OCTANE_PATCHES="$STABLE8_PATCHES"
 
 
 rm -rf fuel-qa8.0
-git_change_request https://github.com/openstack/fuel-qa stable/8.0 fuel-qa8.0  321611
+git_change_request https://github.com/openstack/fuel-qa stable/8.0 fuel-qa8.0  321611 ${FUEL_QA_STABLE8_PATCHES}
 cd fuel-qa8.0
 
 pip install -r fuelweb_test/requirements.txt --upgrade
@@ -59,7 +59,7 @@ unset OCTANE_PATCHES
 
 export OCTANE_PATCHES="$STABLE9_PATCHES"
 rm -rf fuel-qa-mitaka
-git_change_request https://github.com/openstack/fuel-qa stable/mitaka fuel-qa-mitaka 332077 332743
+git_change_request https://github.com/openstack/fuel-qa stable/mitaka fuel-qa-mitaka 332077 332743 ${FUEL_QA_STABLE9_PATCHES}
 
 
 (
