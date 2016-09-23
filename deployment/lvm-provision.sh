db12e39ddb1b6610883f96bf6a3bab866f1347f4
jmikolajczak <jmikolajczak@mirantis.com>
2016-09-23 13:16:13 +0200

    Update playbook and preseed file

--- deployment/lvm-provision.sh
+++ deployment/lvm-provision.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # physical devs
-pvcreate /dev/sdb1 /dev/sdc1
+pvcreate /dev/sdb /dev/sdc
 pvscan
 # volume group
-vgcreate vms /dev/sdb1 /dev/sdc1
+vgcreate vms /dev/sdb /dev/sdc
 vgscan
 # volume for original disk
-lvcreate -L 10T -n images vms /dev/sdc1
+lvcreate -L 10T -n images vms /dev/sdc
 # volume for cache
-lvcreate -L 185G -n images_cache vms /dev/sdb1
+lvcreate -L 185G -n images_cache vms /dev/sdb
 # volume for metadata
-lvcreate -L 512M -n images_cache_meta vms /dev/sdb1
+lvcreate -L 512M -n images_cache_meta vms /dev/sdb
 # create cache-pool
 lvconvert --type cache-pool --cachemode writethrough --poolmetadata vms/images_cache_meta vms/images_cache -y
 # create lv with cache
