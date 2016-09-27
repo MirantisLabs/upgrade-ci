6aa39ba069c949a3973bd0c1706bb2024e52f4a3
jmikolajczak <jmikolajczak@mirantis.com>
2016-09-27 09:05:17 +0200

    Network reconfiguration from dhcp to static
    Also, fixed LVM stall issue

--- deployment/lvm-provision.sh
+++ deployment/lvm-provision.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # physical devs
-pvcreate /dev/sdb /dev/sdc
-pvscan
+pvcreate /dev/sdb /dev/sdc --zero y --yes
 # volume group
-vgcreate vms /dev/sdb /dev/sdc
-vgscan
+vgcreate vms /dev/sdb /dev/sdc --yes
 # volume for original disk
-lvcreate -L 10T -n images vms /dev/sdc
+lvcreate -L 10T -n images vms /dev/sdc --yes
 # volume for cache
-lvcreate -L 185G -n images_cache vms /dev/sdb
+lvcreate -L 185G -n images_cache vms /dev/sdb --yes
 # volume for metadata
-lvcreate -L 512M -n images_cache_meta vms /dev/sdb
+lvcreate -L 512M -n images_cache_meta vms /dev/sdb --yes
 # create cache-pool
-lvconvert --type cache-pool --cachemode writethrough --poolmetadata vms/images_cache_meta vms/images_cache -y
+lvconvert --type cache-pool --cachemode writethrough --poolmetadata vms/images_cache_meta vms/images_cache --yes
 # create lv with cache
-lvconvert --type cache --cachepool vms/images_cache vms/images
+lvconvert --type cache --cachepool vms/images_cache vms/images --yes
 
 touch $1
