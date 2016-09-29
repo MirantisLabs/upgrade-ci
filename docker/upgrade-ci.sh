55289239b74f6a4888e0ad52887450291b9cfe97
Sergey Ryabin <sryabin@mirantis.com>
2016-09-29 18:59:14 +0300

    some code cleanup

--- docker/upgrade-ci.sh
+++ docker/upgrade-ci.sh
@@ -5,6 +5,7 @@ export JENKINS_URL=${JENKINS_URL:-http://localhost:8080}
 export JENKINS_USER=${JENKINS_USER:-admin}
 export JENKINS_PASS=${JENKINS_PASS:-admin}
 export JENKINS_CREDS="--username ${JENKINS_USER} --password ${JENKINS_PASS}"
+export JENKINS_CREDS=""
 export JENKINS_CLI="java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s ${JENKINS_URL}"
 
 
@@ -12,16 +13,16 @@ export JENKINS_CLI="java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s $
 upload_jobs() {
         local git_dir=$1
         test -d ${git_dir}
-        pushd
+#        pushd
         cd ${git_dir}/jj_configs
-        # TODO check if job is alredy exists (jenkins-cli get-job) 
+        # TODO check if job is alredy exists (jenkins-cli get-job)
         find *  -maxdepth 1 -type d -print | xargs -tI% bash -c "${JENKINS_CLI} create-job '%' ${JENKINS_CREDS} < '%/config.xml'"
-        popd
-} 
+#        popd
+}
 
 install_plugins() {
-        xargs -tI% ${JENKINS_CLI} install-plugin % ${JENKINS_CREDS} 
-} 
+        xargs -tI% ${JENKINS_CLI} install-plugin % ${JENKINS_CREDS}
+}
 
 required_plugin_list() {
 cat<<EOF
@@ -31,6 +32,7 @@ greenballs
 nodelabelparameter
 rebuild
 timestamper
+groovy
 EOF
 }
 
@@ -40,3 +42,5 @@ cd /tmp
 git clone http://github.com/mirantislabs/upgrade-ci ${TMP_DIR}
 required_plugin_list | install_plugins
 upload_jobs ${TMP_DIR}
+
+${JENKINS_CLI} restart ${JENKINS_CREDS}
