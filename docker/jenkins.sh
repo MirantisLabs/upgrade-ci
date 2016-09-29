bd80063c3f960ea66f5020fcf0eb6f0c5650d570
Sergey Ryabin <sryabin@mirantis.com>
2016-09-29 19:08:14 +0300

    Implement bootstrap upgrade-ci in jenkins.sh

--- docker/jenkins.sh
+++ docker/jenkins.sh
@@ -19,8 +19,27 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     jenkins_opts_array+=( "$item" )
   done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
 
-  exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
+  exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@" &
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
-exec "$@"
+exec "$@" &
+
+UPGRADE_CI_SCRIPT="/tmp/upgrade-ci.sh"
+
+#ugly (metacoma) rewrite poller and checker
+test -f ${UPGRADE_CI_SCRIPT} && while :; do
+	java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ >/dev/null 2>&1 && {	
+		java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ list-jobs | grep '.*' && break
+		${UPGRADE_CI_SCRIPT}
+		rm -v ${UPGRADE_CI_SCRIPT}
+		break
+	} 
+	# (metacoma) infinity loop? limit retry count
+	sleep 1
+done
+
+# nice nice nice, TODO (metacoma) rewrite
+while :; do
+	sleep 1 # brilliant
+done
